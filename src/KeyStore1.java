2d0c8330f2c39e7594d3b7fbdc83c55dfb6f2fd5
Tejas Kochar <T.Kochar@student.tudelft.nl>
2025-05-14 15:39:59 +0200

    contains method and more tries to prove

--- src/KeyStore1.java
+++ src/KeyStore1.java
@@ -17,13 +17,20 @@ public class KeyStore1 {
     */
 
     // uniqueness of keys
-    /* @
+    /*@
         invariant (\forall int x;
                   0 <= x < a.length;
                         !(\exists int y;
-                        0 <= y < a.length && y != x;
-                        ((Entry)a[x]).key == ((Entry)a[y]).key)
+                        x <= y < a.length;
+                        ((Entry)a[x]).key == ((Entry)a[y]).key) ==> x == y
                   );
+    */
+    // non-null (should be by default...)
+    /*@
+        invariant
+            (\forall int i;
+            0 <= i < entries.length;
+            entries[i] != null && entries[i].key != null && entries[i].value != null);
     */
 
     /*@
@@ -49,9 +56,13 @@ public class KeyStore1 {
                                 0 <= i < counter;
                                 ((Entry)a[i]).key == key);
 
-            decreases entries.length - counter;
+             loop_invariant (\forall int i;
+                              0 <= i < counter;
+                              ((Entry)a[i]).key == key  ==>  res == ((Entry)a[i]).value);
+            assignable \strictly_nothing;
+            decreases a.length - counter;
         */
-        while (counter < entries.length) {
+        while (counter < entries.length) { // as of now, I think proof is not using uniquness part to show that forall holds
             if (key.equals(entries[counter].key)) {
                 res = entries[counter].value;
             }
@@ -66,6 +77,92 @@ public class KeyStore1 {
     void put(Object key, Object value) {}
 
     void remove(Object key) {}
+
+    /*@
+        normal_behaviour
+        requires entries != null;
+        ensures \result == (a.length > 0 && (\exists int i;
+                            0 <= i < a.length;
+                            ((Entry)a[i]).key == key));
+        assignable \strictly_nothing;
+    */
+    boolean /*@ strictly_pure */ contains(Object key) {
+        int counter = 0;
+        boolean res = false;
+        /*@
+            loop_invariant 0 <= counter && (a.length == 0 ==> counter == 0) && (a.length == 0 || counter < a.length);
+            loop_invariant res == (counter > 0 && (\exists int i;
+                            0 <= i < counter;
+                            ((Entry)a[i]).key == key));
+
+            assignable \strictly_nothing;
+            decreases a.length - counter;
+        */
+        while (counter < entries.length) {
+            if (key.equals(entries[counter].key)) {
+                res = true;
+            }
+            counter++;
+        }
+        return res;
+    }
+
+    /*@
+        normal_behaviour
+        requires
+            entries != null;
+        ensures
+            \result == !(\forall int i; 0 <= i < a.length; ((Entry)a[i]).key != key);
+        assignable
+            \strictly_nothing;
+    */
+    boolean /*@ strictly_pure */ contains2(Object key) {
+        int counter = 0;
+        /*@
+            loop_invariant
+                0 <= counter <= a.length;
+            loop_invariant
+                (\forall int i; 0 <= i < counter; ((Entry)a[i]).key != key);
+            assignable \strictly_nothing;
+            decreases a.length - counter;
+        */
+        while (counter < entries.length) {
+            if (key.equals(entries[counter].key)) {
+                return true;
+            }
+            counter++;
+        }
+        return false;
+    }
+
+    /*@
+        normal_behaviour
+        requires
+            entries != null;
+        ensures
+            \result == !(\forall int i; 0 <= i < a.length; ((Entry)a[i]).key != key);
+        assignable
+            \strictly_nothing;
+    */
+    boolean /*@ strictly_pure */ contains3(Object key) {
+        boolean res = false;
+        int counter = 0;
+        /*@
+            loop_invariant
+                0 <= counter <= a.length;
+            loop_invariant
+                res <==> (counter > 0 && (\forall int i; 0 <= i < counter; ((Entry)a[i]).key != key));
+            assignable \strictly_nothing;
+            decreases a.length - counter;
+        */
+        while (counter < entries.length) {
+            if (key.equals(entries[counter].key)) {
+                res = true;
+            }
+            counter++;
+        }
+        return res;
+    }
 }
 
 
