08036d8c8a8517b242cc5931e3500e45d028f649
Tejas Kochar <T.Kochar@student.tudelft.nl>
2025-06-11 16:34:58 +0200

    proving shift seems to gets stuck in an infinite loop in this version

--- src/Sorting/SwapInsertionSort.java
+++ src/Sorting/SwapInsertionSort.java
@@ -11,16 +11,16 @@ public class SwapInsertionSort {
 
 
     /*@
-        normal_behaviour
+        public normal_behaviour
         requires a.length > 0;
-        ensures \dl_seqPerm(seqa, \old(seqa));
-        ensures (\forall int x, y; 0 <= x < a.length && 0 <= y < a.length; x < y ==> a[x] <= a[y]);
+//        ensures \dl_seqPerm(seqa, \old(seqa));
+        ensures (\forall int x; 0 <= x < a.length-1; a[x] <= a[x+1]);
      */
     void sort() {
         /*@
             loop_invariant 1 <= i <= a.length;
-            loop_invariant \dl_seqPerm(seqa, \old(seqa));
-            loop_invariant (\forall int x, y; 0 <= x < i && 0 <= y < i; x < y ==> a[x] <= a[y]);
+//            loop_invariant \dl_seqPerm(seqa, \old(seqa));
+            loop_invariant (\forall int x; 0 <= x < i-1; a[x] <= a[x+1]);
             assignable a[*];
             decreases a.length - i;
          */
@@ -30,40 +30,27 @@ public class SwapInsertionSort {
     }
 
     /*@
-        normal_behaviour
+        public normal_behaviour
         requires 1 <= i < a.length && a.length > 0;
         requires (\forall int x, y; 0 <= x < i && 0 <= y < i; x < y ==> a[x] <= a[y]);
-        ensures \dl_seqPerm(seqa, \old(seqa));
-        ensures (\forall int x, y; 0 <= x <= i && 0 <= y <= i; x < y ==> a[x] <= a[y]);
+//        ensures \dl_seqPerm(seqa, \old(seqa));
+        ensures (\forall int x; 0 <= x < i; a[x] <= a[x+1]);
         assignable a[0..i+1];
      */
     void shift(int i) {
         /*@
             loop_invariant -1 <= j < i;
-            loop_invariant \dl_seqPerm(seqa, \old(seqa));
-            loop_invariant (\forall int x, y; j+1 <= x <= i && j+1 <= y <= i; x < y ==> a[x] <= a[y]);
+//            loop_invariant \dl_seqPerm(\dl_array2seq(a), \dl_array2seq(\old(a)));
+            loop_invariant (\forall int x; j+1 <= x < i; a[x] <= a[x+1]);
             decreases j+1;
             assignable a[0..i+1];
          */
         for (int j = i-1; j >= 0 && a[j] > a[j+1]; j --) {
-            swap(j, j+1);
+            int tmp = a[j+1];
+            a[j+1] = a[j];
+            a[j] = tmp;
         }
     }
-
-    /*@ normal_behaviour
-        requires a.length == 2 && a[0] == 0 && a[1] == 1 && i != j && 0 <= i < a.length && 0 <= j < a.length;
-//        ensures \dl_seqPerm(seqa, \old(seqa));
-        ensures \dl_seqPerm(\dl_array2seq(a), \dl_array2seq(\old(a)));
-        ensures \old(a) == a;
-        ensures a[1] == 0 && a[0] == 1;
-        assignable a[i], a[j];
-     */
-    void swap(int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    }
-//        requires a != null && 0 <= i < a.length && 0 <= j < a.length && a.length > 0 && i != j;
 }
 
 
